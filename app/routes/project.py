f346a3907323d52e6e719d24688a6970fa15c522
Zainisrar <zainisrar2003@gmail.com>
2025-08-15 11:56:29 +0500

    update

--- app/routes/project.py
+++ app/routes/project.py
@@ -51,7 +51,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-   region_name="eu-central-1"
+   region_name="eu-north-1"
 )
 
 bucket_name = "ai-auto-invoice"
